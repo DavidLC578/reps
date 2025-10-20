d6346ab15d3ec812a0f569724179418e43d7b0eb
DavidLC578 <davidlatt2002@gmail.com>
2025-10-20 16:25:17 +0200

    add check icon

--- components/ExerciseCard.jsx
+++ components/ExerciseCard.jsx
@@ -1,14 +1,28 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { Check } from "./Icons";
+import { useState } from "react";
 
 export default function ExerciseCard({ exercise, index }) {
+  const [checked, setChecked] = useState(false);
   return (
     <View style={styles.container}>
-      <Text style={styles.exerciseNumber}>Ejercicio {index + 1}</Text>
-      <Text style={styles.exerciseName}>{exercise.name}</Text>
-      <View style={styles.details}>
-        <Text>{exercise.sets} series</Text>
-        <Text>{exercise.reps} repeticiones</Text>
-        {exercise.weight && <Text>{exercise.weight} kg</Text>}
+      <View>
+        <Text style={styles.exerciseNumber}>Ejercicio {index + 1}</Text>
+        <Text style={styles.exerciseName}>{exercise.name}</Text>
+        <View style={styles.details}>
+          <Text>{exercise.sets} series</Text>
+          <Text>{exercise.reps} repeticiones</Text>
+          {exercise.weight && <Text>{exercise.weight} kg</Text>}
+        </View>
+      </View>
+      <View style={styles.checkContainer}>
+        <Pressable onPress={() => setChecked(!checked)}>
+          {checked ? (
+            <Check style={styles.isChecked} />
+          ) : (
+            <Check style={styles.check} />
+          )}
+        </Pressable>
       </View>
     </View>
   );
@@ -41,5 +55,24 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     gap: 10,
     marginTop: 6,
+  },
+  checkContainer: {
+    position: "absolute",
+    right: 15,
+    top: 15,
+  },
+  check: {
+    borderWidth: 2,
+    borderColor: "#000",
+    borderRadius: 50,
+    backgroundColor: "#fff",
+    padding: 5,
+  },
+  isChecked: {
+    borderWidth: 2,
+    borderColor: "#607afb",
+    borderRadius: 50,
+    backgroundColor: "#607afb",
+    padding: 5,
   },
 });
---
+++ components/Icons.jsx
@@ -0,0 +1,5 @@
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+export const Check = (props) => (
+  <AntDesign name="check" size={20} color="black" {...props} />
+);
